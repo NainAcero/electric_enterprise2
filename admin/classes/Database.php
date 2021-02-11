345237b92df984e8f9756274ff68a2f76a050d8b
NainAceroMamani <a_nacerom@unjbg.edu.pe>
2021-02-11 15:26:08 -0500

    update 11/02/2021

--- admin/classes/Database.php
+++ admin/classes/Database.php
@@ -8,7 +8,7 @@ class Database
 	
 	private $con;
 	public function connect(){
-		$this->con = new Mysqli("localhost", "root", "", "electric_enterprise");
+		$this->con = new Mysqli("bcrpyeszazfgqo2x2s7b-mysql.services.clever-cloud.com", "umecbng9usujxe2v", "rjqUNYc2cmUzQGKNPuTL", "bcrpyeszazfgqo2x2s7b");
 		return $this->con;
 	}
 }
