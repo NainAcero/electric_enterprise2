d9723ac505a817605e1875ed128f30d2ff6b5478
NainAceroMamani <a_nacerom@unjbg.edu.pe>
2021-02-11 12:33:46 -0500

    first commit

---
+++ admin/classes/Customers.php
@@ -0,0 +1,111 @@
+<?php 
+session_start();
+/**
+ * 
+ */
+class Customers
+{
+	
+	private $con;
+	private $id_user;
+
+	function __construct()
+	{
+		include_once("Database.php");
+		$db = new Database();
+		$this->con = $db->connect();
+	}
+
+	public function getCustomers(){
+		$query = $this->con->query("SELECT `user_id`, `first_name`, `last_name`, `email`, `mobile`, `address1`, `address2` FROM `user_info`");
+		$ar = [];
+		if (@$query->num_rows > 0) {
+			while ($row = $query->fetch_assoc()) {
+				$ar[] = $row;
+			}
+			return ['status'=> 202, 'message'=> $ar];
+		}
+		return ['status'=> 303, 'message'=> 'sin datos de clientes'];
+	}
+
+
+	public function getCustomersOrder(){
+		$query = $this->con->query("SELECT o.user_id , o.order_id, o.product_id, o.qty, o.trx_id, o.p_status, p.product_title, p.product_image FROM orders o JOIN products p ON o.product_id = p.product_id");
+		$ar = [];
+		if (@$query->num_rows > 0) {
+			while ($row = $query->fetch_assoc()) {
+				$ar[] = $row;
+			}
+			return ['status'=> 202, 'message'=> $ar];
+		}
+		return ['status'=> 303, 'message'=> 'aún no hay pedidos'];
+	}
+
+	public function getUserInfo(){
+		$query = $this->con->query("SELECT * FROM user_info where user_id = $this->id_user");
+		$ar = [];
+		if (@$query->num_rows > 0) {
+			while ($row = $query->fetch_assoc()) {
+				$ar[] = $row;
+			}
+			return ['status'=> 202, 'message'=> $ar[0]];
+		}
+		return ['status'=> 303, 'message'=> 'aún no hay pedidos'];
+	}
+
+	public function sendEmail(){
+
+		$from = "nain.acero24@gmail.com";
+		$to = "a_nacerom@unjbg.edu.pe";
+		$subject = "REGISTRO CIIS XXI";
+		$cabeceras  = 'MIME-Version: 1.0' . "\r\n";
+		$cabeceras .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+		$cabeceras .= 'From: ' . $from;
+		$mensaje = 'hola';
+		mail($to,$subject,$mensaje, $cabeceras);
+
+		// $userInfo =  $this->getUserInfo();
+		// $mail =  $userInfo["message"]["email"];
+
+		// return $mail;
+	}
+	
+	public function setIdUser($id_user){
+		$this->id_user = $id_user;
+	}
+
+}
+
+
+/*$c = new Customers();
+echo "<pre>";
+print_r($c->getCustomers());
+exit();*/
+
+if (isset($_POST["GET_CUSTOMERS"])) {
+	if (isset($_SESSION['admin_id'])) {
+		$c = new Customers();
+		echo json_encode($c->getCustomers());
+		exit();
+	}
+}
+
+if (isset($_POST["GET_CUSTOMER_ORDERS"])) {
+	if (isset($_SESSION['admin_id'])) {
+		$c = new Customers();
+		echo json_encode($c->getCustomersOrder());
+		exit();
+	}
+}
+
+if(isset($_POST["GET_CUSTOMER_ORDERS_EMAIL"])){
+	$id_user = $_POST["GET_CUSTOMER_ORDERS_EMAIL"];
+	$c = new Customers();
+	$c->setIdUser($id_user);
+	$data = $c->sendEmail();
+	echo json_encode($data);
+	exit();
+}
+
+
+?>
---
+++ admin/js/customers.js
@@ -0,0 +1,93 @@
+$(document).ready(function(){
+
+	getCustomers();
+	getCustomerOrders();
+
+	function getCustomers(){
+		$.ajax({
+			url : '../admin/classes/Customers.php',
+			method : 'POST',
+			data : {GET_CUSTOMERS:1},
+			success : function(response){
+				
+				console.log(response);
+				var resp = $.parseJSON(response);
+				if (resp.status == 202) {
+
+					var customersHTML = "";
+
+					$.each(resp.message, function(index, value){
+
+						customersHTML += '<tr>'+
+									          '<td>#</td>'+
+									          '<td>'+value.first_name+' '+value.last_name+'</td>'+
+									          '<td>'+value.email+'</td>'+
+									          '<td>'+value.mobile+'</td>'+
+									          '<td>'+value.address1+'<br>'+value.address2+'</td>'+
+									       '</tr>'
+
+					});
+
+					$("#customer_list").html(customersHTML);
+
+				}else if(resp.status == 303){
+
+				}
+
+			}
+		})
+		
+	}
+
+	function getCustomerOrders(){
+		$.ajax({
+			url : '../admin/classes/Customers.php',
+			method : 'POST',
+			data : {GET_CUSTOMER_ORDERS:1},
+			success : function(response){
+				
+				console.log(response);
+				var resp = $.parseJSON(response);
+				if (resp.status == 202) {
+
+					var customerOrderHTML = "";
+
+					$.each(resp.message, function(index, value){
+
+						customerOrderHTML +='<tr>'+
+								              '<td>#</th>'+
+								              '<td>'+ value.order_id +'</td>'+
+								              '<td>'+ value.product_id +'</td>'+
+								              '<td>'+ value.product_title +'</td>'+
+								              '<td>'+ value.qty +'</td>'+
+								              '<td>'+ value.trx_id +'</td>'+
+								              '<td>'+ value.p_status +'</td>'+
+								              '<td><button onclick="enviarcorreo('+ value.user_id +')" class="btn btn-warning text-white"><i class="far fa-envelope"></i></button></td>'+
+								            '</tr>';
+						console.log(value);
+					});
+
+					$("#customer_order_list").html(customerOrderHTML);
+
+				}else if(resp.status == 303){
+					$("#customer_order_list").html(resp.message);
+				}
+
+			}
+		})
+		
+	}
+
+
+});
+
+function enviarcorreo($data){
+	$.ajax({
+		url : '../admin/classes/Customers.php',
+		method : 'POST',
+		data : {GET_CUSTOMER_ORDERS_EMAIL:$data},
+		success : function(response){
+			console.log(response);
+		}
+	})
+}
